ae74c518565dc9063bbe28d918b406005590ffcd
michel3778 <124093915+michel3778@users.noreply.github.com>
2023-02-02 11:01:41 -0300

    ee

--- solicitacao.js
+++ solicitacao.js
@@ -1,2 +1,9 @@
 console.log(foi, foi, foi, foi eleeeeeaassssaaa!);
-console.log('dddddddfffffdddddddxxxxxxxx');
+console.log('dddddddfffffddddddddxxxxxxxx');console.log(foi, foi, foi, foi eleeeeeaassssaaa!);
+console.log('dddddddfffffddddddddxxxxxxxx');console.log(foi, foi, foi, foi eleeeeeaassssaaa!);
+console.log('dddddddfffffddddddddxxxxxxxx');console.log(foi, foi, foi, foi eleeeeeaassssaaa!);
+console.log('dddddddfffffddddddddxxxxxxxx');console.log(foi, foi, foi, foi eleeeeeaassssaaa!);
+console.log('dddddddfffffddddddddxxxxxxxx');console.log(foi, foi, foi, foi eleeeeeaassssaaa!);
+console.log('dddddddfffffddddddddxxxxxxxx');console.log(foi, foi, foi, foi eleeeeeaassssaaa!);
+console.log('dddddddfffffddddddddxxxxxxxx');console.log(foi, foi, foi, foi eleeeeeaassssaaa!);
+console.log('dddddddfffffddddddddxxxxxxxx');
