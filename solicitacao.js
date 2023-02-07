e18fe3168d231fdbef7ccf354ebc6070a9d6984c
michel3778 <124093915+michel3778@users.noreply.github.com>
2023-02-07 15:20:27 -0300

    asdf

--- solicitacao.js
+++ solicitacao.js
@@ -125,8 +125,19 @@ inputCnpj.oninput = function() {
     //refreshTags();
 }
 
-function cnpjMask (phone) {
-    return phone.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
+function cnpjMask (v) {
+    v=v.replace(/^(\d{2})(\d)/,"$1.$2");
+ 
+    //Coloca ponto entre o quinto e o sexto dígitos
+    v=v.replace(/^(\d{2})\.(\d{3})(\d)/,"$1.$2.$3");
+
+    //Coloca uma barra entre o oitavo e o nono dígitos
+    v=v.replace(/\.(\d{3})(\d)/,".$1/$2");
+
+    //Coloca um hífen depois do bloco de quatro dígitos
+    v=v.replace(/(\d{4})(\d)/,"$1-$2");
+
+    return v;
 };
 
 
