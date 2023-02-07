eb18cea2e8d65114cca695986a216cb55fed7af6
michel3778 <124093915+michel3778@users.noreply.github.com>
2023-02-07 15:10:08 -0300

    sdf

--- solicitacao.js
+++ solicitacao.js
@@ -127,7 +127,7 @@ inputCnpj.oninput = function() {
 
 function cnpjMask (phone) {
     return phone.replace(/\D/g, '')
-        .replace(/^(\d{2})(\d{3})?(\d{3})?(\d{4})?(\d{2})?/, "$1 $2 $3/$4-$5"));
+        .replace(/^(\d{2})(\d{3})?(\d{3})?(\d{4})?(\d{2})?/, "$1 $2 $3/$4-$5");
 };
 
 
