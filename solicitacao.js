7b4efd6b847ca926f12181ec6e3339e9e21e6797
michel3778 <124093915+michel3778@users.noreply.github.com>
2023-02-08 20:23:11 -0300

    1234

--- solicitacao.js
+++ solicitacao.js
@@ -119,9 +119,9 @@ refreshTags();
 
 //CNPJ >>>>>>>>>>
 //inputCnpj.oninput = function() {
-inputCnpj.addEventListener("click", () => {
+inputCnpj.addEventListener("click", (e) => {
     console.log('CNPJ');
-    var x = this.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,3})(\d{0,3})(\d{0,4})(\d{0,2})/);
+    var x = e.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,3})(\d{0,3})(\d{0,4})(\d{0,2})/);
     this.value = !x[2] ? x[1] : x[1] + '.' + x[2] + '.' + x[3] + '/' + x[4] + (x[5] ? '-' + x[5] : '');
 });
 
