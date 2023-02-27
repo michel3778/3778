e8540bb3007eff7dc52d6d3948ae9d16866e35ba
michel3778 <124093915+michel3778@users.noreply.github.com>
2023-02-27 17:41:13 -0300

    asdf

--- solicitacao.js
+++ solicitacao.js
@@ -429,8 +429,8 @@ function resizear() {
 	oSlider.addEventListener('input', function onInput(event) {
 		if (event.target.id == 'cnpj') {
 			console.log('É o CNPJ')
-			var x = event.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,3})(\d{0,3})(\d{0,4})(\d{0,2})/);
-		    event.value = !x[2] ? x[1] : x[1] + '.' + x[2] + '.' + x[3] + '/' + x[4] + (x[5] ? '-' + x[5] : '');
+			var x = event.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,3})(\d{0,3})(\d{0,4})(\d{0,2})/);
+		    event.target.value = !x[2] ? x[1] : x[1] + '.' + x[2] + '.' + x[3] + '/' + x[4] + (x[5] ? '-' + x[5] : '');
 		};
 		if (event.target.classList.contains('select-estados')) { setMunicipios(e.target.value);	};
 	});
